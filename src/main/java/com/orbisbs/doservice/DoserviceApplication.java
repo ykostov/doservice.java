cbaeed0de965ccd50a3390ec69ec5e5435d4ee4b
Yasen Kostov <qskatak04@gmail.com>
2022-08-22 10:58:03 +0300

    Initial Commit

---
+++ src/main/java/com/orbisbs/doservice/DoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.orbisbs.doservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orbisbs/doservice/DoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbisbs.doservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
