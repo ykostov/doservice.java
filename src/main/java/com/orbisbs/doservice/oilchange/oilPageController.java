9e331c04c4885b921a46f430949476913f523df9
Yasen Kostov <qskatak04@gmail.com>
2022-08-25 14:56:52 +0300

    add logic and return result if car needs oil recharge

--- src/main/java/com/orbisbs/doservice/oilchange/oilPageController.java
+++ src/main/java/com/orbisbs/doservice/oilchange/oilPageController.java
@@ -1,15 +1,9 @@
 package com.orbisbs.doservice.oilchange;
 
 import com.orbisbs.doservice.cars.Car;
+import com.orbisbs.doservice.cars.CarService;
 import com.orbisbs.doservice.oil.Oil;
-import com.orbisbs.doservice.users.User;
-import com.orbisbs.doservice.users.UserDto;
-import com.orbisbs.doservice.users.UserRepository;
-import com.orbisbs.doservice.users.UserService;
 import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,39 +14,46 @@ import java.util.*;
 @AllArgsConstructor
 public class oilPageController {
 
-    private final UserRepository userRepository;
-    private final UserService userService;
-    private final ModelMapper modelMapper;
-    HashMap<Long, String> cars = new HashMap<>();
-    HashMap<Long, String> carsOilFrequency = new HashMap<>();
-    HashMap<Long, String> carsOil = new HashMap<>();
+    private final CarService carService;
+    private HashMap<String, Integer> carsOil;
+    private LinkedHashMap<String, Integer> sortedMap;
 
 
-    @RequestMapping("/user/{id}/cars")
-    public String getUser(@PathVariable Long id) {
-        getCarsMileageAndOilChanges(id);
-        return "Cars for user: " + cars.toString() +
-                " Oil for cars: " + carsOil.toString() +
-                " Oil Frequency for cars: " + carsOilFrequency.toString();
+    @RequestMapping("/oil_change/car/{carId}")
+    public String getUser(@PathVariable Long carId) {
+        this.carsOil.clear();
+        this.sortedMap.clear();
+        Car car = carService.getCar(carId);
+
+        getCarsMileageAndOilChanges(car);
+        String carCurrentMileage = car.getMileage();
+        String carOilFrequency = car.getOilFrequency();
+        return calculateStatusOfCarOilChange(carCurrentMileage, carOilFrequency, car.getModel());
 
     }
 
-    private void getCarsMileageAndOilChanges(Long id) {
-        User user = userService.getUser(id);
-        UserDto userResponse = modelMapper.map(user, UserDto.class);
-        for (Car c : userResponse.getCars() ) {
-            cars.put(c.getId(), c.getMileage());
-            carsOilFrequency.put(c.getId(), c.getOilFrequency());
-            for(Oil o : c.getOil()) {
-                carsOil.put(c.getId(), o.getMileage());
+    private void getCarsMileageAndOilChanges(Car car) {
+            for(Oil o : car.getOil()) {
+                carsOil.put(o.getDate(), Integer.parseInt(o.getMileage()));
             }
-        }
+
     }
 
-    private void calculateStatusOfCarOilChange() {
-        for (int i = 0; i < cars.size(); i++) {
-            // do something funny
+    private String calculateStatusOfCarOilChange(String mileage, String frequency, String model) {
+
+        carsOil.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
+        for (var entry : sortedMap.entrySet()) {
+
+            if ((Integer.parseInt(mileage) - entry.getValue()) >  Integer.parseInt(frequency))
+            {
+                return "you need to change the oil! Last oil changed at: " + entry.getValue() +
+                "km." + "\r\n" + "You have passed the line of oil frequency with: " + ((Integer.parseInt(mileage) - entry.getValue()) - Integer.parseInt(frequency)) + "km! Change the oil as soon as possible!";
+
+            }
+            return "Everything is in order";
+
         }
+        return "No data";
     }
 
 
