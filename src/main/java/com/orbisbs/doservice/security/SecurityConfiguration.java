808837246a7aa51e3a584650668a4ee0a5af0044
Yasen Kostov <qskatak04@gmail.com>
2022-08-25 13:32:03 +0300

    configure++ security

--- src/main/java/com/orbisbs/doservice/security/SecurityConfiguration.java
+++ src/main/java/com/orbisbs/doservice/security/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -26,15 +27,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-         return NoOpPasswordEncoder.getInstance();
+         return new BCryptPasswordEncoder();
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/cars").hasRole("ADMIN")
-                .antMatchers("/users").hasAnyRole("USER", "ADMIN")
-                .and().formLogin();
+//        http.authorizeRequests()
+//                .antMatchers("/cars").hasRole("ADMIN")
+//                .antMatchers("/users").hasAnyRole("USER", "ADMIN")
+//                .and().formLogin();
     }
 
 }
