a22aac300779981eaf37c1809b9014d18943c678
Yasen Kostov <qskatak04@gmail.com>
2022-08-26 13:39:45 +0300

    create Auth Requests and Responses

---
+++ src/main/java/com/orbisbs/doservice/models/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.orbisbs.doservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+}
