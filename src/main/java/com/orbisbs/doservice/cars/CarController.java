1459f55235314a3a2d50f8755e2aa44a405ab436
Yasen Kostov <qskatak04@gmail.com>
2022-08-29 11:36:43 +0300

    add Swagger annotations in CarController

--- src/main/java/com/orbisbs/doservice/cars/CarController.java
+++ src/main/java/com/orbisbs/doservice/cars/CarController.java
@@ -3,6 +3,12 @@ package com.orbisbs.doservice.cars;
 import com.orbisbs.doservice.oil.OilDto;
 import com.orbisbs.doservice.users.User;
 import com.orbisbs.doservice.users.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
@@ -15,13 +21,25 @@ import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
+@Tag(name="Car CRUD", description = "Create / Remove / Update / Delete Car")
+@RequestMapping("/cars")
 public class CarController {
 
 
     private final CarService carService;
     private final ModelMapper modelMapper;
 
-    @RequestMapping("/cars/{id}")
+    @Operation(summary = "Get a car by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found a car",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Car not found",
+                    content = @Content) })
+
+    @GetMapping("/{id}")
     public ResponseEntity<CarDto> getCar(@PathVariable Long id) {
         Car car = carService.getCar(id);
         // convert entity to DTO
@@ -30,12 +48,14 @@ public class CarController {
         return ResponseEntity.ok().body(carResponse);
     }
 
-    @RequestMapping("/cars")
+    @Operation(summary = "Get all cars")
+    @GetMapping
     public List<CarDto> getAllCars() {
         return carService.getAllCars().stream().map(car -> modelMapper.map(car, CarDto.class)).collect(Collectors.toList());
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/cars")
+    @Operation(summary = "Add an Car")
+    @PostMapping
     public ResponseEntity<CarDto> addCar(@Valid @RequestBody CarDto carDto) {
 
         Car carRequest = modelMapper.map(carDto, Car.class);
@@ -45,7 +65,8 @@ public class CarController {
         return new ResponseEntity<CarDto>(carResponse, HttpStatus.CREATED);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/cars/{id}")
+    @Operation(summary = "update a Car by its id")
+    @PutMapping("/{id}")
     public ResponseEntity<CarDto> updateCar(@RequestBody CarDto carDto, @PathVariable Long id) {
 
         // convert DTO to Entity
@@ -56,7 +77,8 @@ public class CarController {
         return ResponseEntity.ok().body(carResponse);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/cars/{id}")
+    @Operation(summary = "delete a Car by its id")
+    @DeleteMapping("/{id}")
     public void deleteCar(@PathVariable Long id) {
         carService.deleteCar(id);
     }
