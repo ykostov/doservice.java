8a4633046f456ad29b0b2be62995b4c5c5cf7ed0
Yasen Kostov <qskatak04@gmail.com>
2022-08-29 16:26:12 +0300

    improve security config using jtw token

--- src/main/java/com/orbisbs/doservice/security/SecurityConfiguration.java
+++ src/main/java/com/orbisbs/doservice/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.orbisbs.doservice.filters.JwtRequestFilter;
 import com.orbisbs.doservice.models.AuthenticationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,13 +12,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @AllArgsConstructor
-
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final MyUserDetailsService myUserDetailsService;
@@ -43,20 +43,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
-//        http.csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/cars").hasRole("ADMIN")
-//                .antMatchers("/users").hasRole("ADMIN")
-//                .and().httpBasic();
 
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/v3/api-docs/**").permitAll()
                 .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/cars").permitAll()
+                .antMatchers("/oil_change/**").permitAll()
                 .antMatchers("/auth").permitAll().anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+
 
     }
 
