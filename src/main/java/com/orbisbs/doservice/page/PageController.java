731be8170448cf2c2af85776f7270e610a477ff9
Yasen Kostov <qskatak04@gmail.com>
2022-09-05 13:22:22 +0300

    remove unnecessary imports

--- src/main/java/com/orbisbs/doservice/page/PageController.java
+++ src/main/java/com/orbisbs/doservice/page/PageController.java
@@ -21,6 +21,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
