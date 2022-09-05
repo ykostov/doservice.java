78a3fc144aa7943205b70b1b480df7485d0eb91c
Yasen Kostov <qskatak04@gmail.com>
2022-09-05 13:20:23 +0300

    create addCar and showCars forms

--- src/main/java/com/orbisbs/doservice/cars/CarController.java
+++ src/main/java/com/orbisbs/doservice/cars/CarController.java
@@ -13,16 +13,19 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
+@Controller
 @AllArgsConstructor
 @Tag(name="Car CRUD", description = "Create / Remove / Update / Delete Car")
 @RequestMapping("/cars")
@@ -55,31 +58,45 @@ public class CarController {
 
     @Operation(summary = "Get all cars")
     @GetMapping
-    public List<CarDto> getAllCars() {
-        return carService.getAllCars().stream().map(car -> modelMapper.map(car, CarDto.class)).collect(Collectors.toList());
+    public String getAllCars(Model model) {
+        List<CarDto> list = carService.getAllCars().stream().map(car -> modelMapper.map(car, CarDto.class)).collect(Collectors.toList());
+        model.addAttribute("cars", list);
+        return "cars";
+    }
+
+    @GetMapping("/new")
+    public String createCarForm(Model model) {
+        Car car = new Car();
+        model.addAttribute("car", car);
+        return "create_car";
     }
 
     @Operation(summary = "Add an Car")
     @PostMapping
-    public ResponseEntity<CarDto> addCar(@Valid @RequestBody CarDto carDto) {
+    public String addCar(@ModelAttribute("car") CarDto carDto) {
 
         Car carRequest = modelMapper.map(carDto, Car.class);
         Car car = carService.addCar(carRequest);
-        // convert entity to DTO
-        CarDto carResponse = modelMapper.map(car, CarDto.class);
-        return new ResponseEntity<CarDto>(carResponse, HttpStatus.CREATED);
+
+        return "redirect:/cars";
     }
 
     @Operation(summary = "update a Car by its id")
-    @PutMapping("/{id}")
-    public ResponseEntity<CarDto> updateCar(@RequestBody CarDto carDto, @PathVariable Long id) {
+    @PostMapping("/{id}")
+    public String updateCar(@PathVariable Long id, @ModelAttribute("car") CarDto carDto, Model model) {
 
         // convert DTO to Entity
         Car carRequest = modelMapper.map(carDto, Car.class);
         Car car = carService.updateCar(id, carRequest);
         // entity to DTO
         CarDto carResponse = modelMapper.map(car, CarDto.class);
-        return ResponseEntity.ok().body(carResponse);
+        return "redirect:/cars";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editCarForm(@PathVariable Long id, Model model) {
+        model.addAttribute("car", carService.getCar(id));
+        return "edit_car";
     }
 
     @Operation(summary = "delete a Car by its id")
